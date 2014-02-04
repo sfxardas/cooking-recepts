3fd09ddfd29adc45bb4ea1541fe3fc2f2af14500
sfxardas <hochzeittsw@gmail.com>
2014-02-04 17:56:06 +0100

    Initial commit

---
+++ src/main/java/org/xardas/db/DataStorage.java
@@ -0,0 +1,7 @@
+package org.xardas.db;
+
+/**
+ * Created by Sebastian on 04.02.14.
+ */
+public class DataStorage {
+}
---
+++ src/main/java/org/xardas/recepts/Recept.java
@@ -0,0 +1,7 @@
+package org.xardas.recepts;
+
+/**
+ * Created by Sebastian on 04.02.14.
+ */
+public class Recept {
+}
